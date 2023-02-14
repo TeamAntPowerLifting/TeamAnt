4bd840d2bb00cae27b0de8c46a0560f24845aeb0
1ilkwon <dlfrnjs318@gmail.com>
2023-02-14 23:25:22 +0900

    feat : add send email
    add send email

--- frontend/src/pages/AntContact.tsx
+++ frontend/src/pages/AntContact.tsx
@@ -1,48 +1,65 @@
 import React from 'react';
 import Headers from '@/components/headers/header';
 import Footer from '@/components/footers/footer';
+import emailjs from '@emailjs/browser';
+
 
 const AntContact = () => {
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_5qlpg9b', 'template_gwkutmb',
+      e.currentTarget, 'ylQ3THXuqiWqMF9nU')
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+    e.currentTarget.reset()
+  };
+
   return (
     <>
       <div>
         <Headers />
       </div>
       <p >Contact Us</p>
-      <form className="shadow-md w-full md:w-2/3 mx-auto mt-4">
-        <div className="flex flex-wrap -mx-3 mb-6">
-          <div className="w-full px-3">
-            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" v-for="grid-first-name">
-              이름
-            </label>
-            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" />
+      <section>
+        <form className="shadow-md w-full md:w-2/3 mx-auto mt-4">
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full px-3">
+              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" v-for="grid-first-name">
+                이름
+              </label>
+              <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" type="text" placeholder='이름' name='user_name' required />
+            </div>
           </div>
-        </div>
-        <div className="flex flex-wrap -mx-3 mb-6">
-          <div className="w-full px-3">
-            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" v-for="grid-password">
-              이메일
-            </label>
-            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" />
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full px-3">
+              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" v-for="grid-password">
+                이메일
+              </label>
+              <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type='email' placeholder='이메일' name='user_email' required />
+            </div>
           </div>
-        </div>
-        <div className="flex flex-wrap -mx-3 mb-6">
-          <div className="w-full px-3">
-            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" v-for="grid-password">
-              질문 내용
-            </label>
-            <textarea className=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none" id="message"></textarea>
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full px-3">
+              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" v-for="grid-password">
+                질문 내용
+              </label>
+              <textarea className=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none" id="message"></textarea>
+            </div>
           </div>
-        </div>
-        <div className="md:flex md:items-center">
-          <div className="md:w-1/3">
-            <button className="shadow bg-gray-300 hover:bg-red-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
-              전송하기
-            </button>
+          <div className="md:flex md:items-center">
+            <div className="md:w-1/3">
+              <button name='message' className="shadow bg-gray-300 hover:bg-red-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit" value="Send">
+                전송하기
+              </button>
+            </div>
+            <div className="md:w-2/3"></div>
           </div>
-          <div className="md:w-2/3"></div>
-        </div>
-      </form>
+        </form>
+      </section>
 
 
       <p >ANSWERS TO FREQUENTLY ASKED QUESTIONS</p>
