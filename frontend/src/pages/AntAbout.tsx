98797af86fc010c5539140123257edd0cc622999
1ilkwon <dlfrnjs318@gmail.com>
2023-02-15 22:48:48 +0900

    feat : add aboutus img + page
    add aboutus img + page

--- frontend/src/pages/AntAbout.tsx
+++ frontend/src/pages/AntAbout.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import Headers from '@/components/headers/header';
 import Footer from '@/components/footers/footer';
+import about from '../assets/images/aboutus.png';
+
+
 const AntAbout = () => {
+
+
   return (
-    <div className="flex flex-col h-screen">
-      <Headers />
-      <div className="flex-1"></div>
-      <Footer />
-    </div>
+    <>
+      <div className="flex flex-col h-screen">
+        <Headers />
+        <div className="flex-1">
+
+          <img className='object-cover ' src={about} alt=""></img>
+          <h2 className='--text-center'>우리가 어떠한 팀이고, 어떠한 비전과 가치관을 지니고 있는지 소개해드립니다.</h2>
+        </div>
+
+
+
+
+        <Footer />
+      </div>
+    </>
   );
 };
 
