9482f0ffa52b396039cf885953c994cc9754259a
지윤 <dlwldbs369@tukorea.ac.kr>
2023-02-17 18:29:25 +0900

    fix: map key 추가

--- frontend/src/components/coaches/coache.tsx
+++ frontend/src/components/coaches/coache.tsx
@@ -6,8 +6,8 @@ const coache = (props: any) => {
       <div className="mt-8 bg-white mb-28 shadow-lg left-0 p-6 border mt-2 border-indigo-300 rounded-lg mx-auto flex">
         <img className="w-1/5 rounded-lg  ml-19" src={props.image} alt="" />
         <div className="ml-10">
-          {props.title.map((title: any) => (
-            <div>{title}</div>
+          {props.title.map((title: any, idx: any) => (
+            <div key={idx}>{title}</div>
           ))}
         </div>
       </div>
