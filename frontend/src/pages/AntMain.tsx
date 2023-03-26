2f7a6d44350c8cfecc68f3e5937842011b443445
1ilkwon <84134727+1ilkwon@users.noreply.github.com>
2023-03-26 20:01:12 +0900

    fix : main_1 반응형

--- frontend/src/pages/AntMain.tsx
+++ frontend/src/pages/AntMain.tsx
@@ -53,51 +53,50 @@ const AntMain = () => {
           />
         </div>
 
-        <section className="py-12 sm:py-20">
-          <div className="">
-            <img
-              className="object-fill mx-auto rounded-lg sm:h-[100%] h-52  lg:w-2/3 w-[100%]"
-              src={main1}
-              alt=""
-            />
-          </div>
-        </section>
 
-        <section className="py-12 sm:py-20 lg:px-60">
+
+        <section className="py-12 sm:py-20 sm:px-20 lg:px-60">
           <div className="flex mx-auto lg-max-w-full">
             <div className="w-[520px]">
               <div className="flex justify-end">
                 {' '}
                 <img
-                  className="object-cover rounded-lg inset-y-0 h-1/2 "
+                  className="object-cover inset-y-0 md:h-72 sm:h-52 h-40"
                   src={main1_1}
                   alt=""
                 />
               </div>
 
-              <div className=" text-lg font-extrabold text-gray-800  lg:text-xl sm:w-auto w-235 text-right">
-                <p>파워리프팅 아시아 1등</p>
-                <p>아시아 스쿼트 신기록 보유자</p>
-                <p>국내 최초 파워리프팅 국제시합 그랜드 슬램 달성</p>
+              <div className="font-extrabold text-gray-800 w-auto w-235 text-right">
+                <p className="para break-normal  font-extrabold text-gray-800 capitalize text-xs sm:text-base md:text-lg lg:text-xl sm:w-auto"
+                style={{ textShadow: '1.5px 1.5px 2px #C0C0C0' }}>파워리프팅 아시아 1등</p>
+                <p className="para break-normal  font-extrabold text-gray-800 capitalize text-xs sm:text-base md:text-lg lg:text-xl sm:w-auto"
+                style={{ textShadow: '1.5px 1.5px 2px #C0C0C0' }}>아시아 스쿼트 신기록 보유자</p>
+                <p className="para break-normal  font-extrabold text-gray-800 capitalize text-xs sm:text-base md:text-lg lg:text-xl sm:w-auto"
+                style={{ textShadow: '1.5px 1.5px 2px #C0C0C0' }}> 국내 최초 파워리프팅 국제시합 그랜드 슬램 달성</p>
               </div>
             </div>
 
             <div className="items-center justify-center flex">
               <img
-                className="object-fill mx-auto rounded-lg  mx-10 w-56"
+                className="object-fill mx-auto rounded-lg  mx-8 w-44"
                 src={main1_logo}
                 alt=""
               />
             </div>
 
             <div className="w-[450px] mt-10">
-              <div className="text-lg font-extrabold text-gray-800 lg:text-xl sm:w-auto">
-                <p>선수로서 증명해낸 결과와 실력</p>
-                <p>모든 코치진의 국제시합 경험</p>
-                <p>풍부한 티칭경험과 레퍼런스</p>
+            <div className="font-extrabold text-gray-800 w-auto w-235">
+                <p className="para break-normal  font-extrabold text-gray-800 capitalize text-xs sm:text-base md:text-lg lg:text-xl sm:w-auto"
+                style={{ textShadow: '1.5px 1.5px 2px #C0C0C0' }}>선수로서 증명해낸 결과와 실력</p>
+                <p className="para break-normal  font-extrabold text-gray-800 capitalize text-xs sm:text-base md:text-lg lg:text-xl sm:w-auto"
+                style={{ textShadow: '1.5px 1.5px 2px #C0C0C0' }}>모든 코치진의 국제시합 경험</p>
+                <p className="para break-normal  font-extrabold text-gray-800 capitalize text-xs sm:text-base md:text-lg lg:text-xl sm:w-auto"
+                style={{ textShadow: '1.5px 1.5px 2px #C0C0C0' }}>풍부한 티칭경험과 레퍼런스</p>
               </div>
+
               <img
-                className="object-fill mx-auto rounded-lg h-1/"
+                className="object-fill mx-auto xl:h-4/5 lg:5/2 sm: 2/5"
                 src={main1_2}
                 alt=""
               />
